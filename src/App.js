52cd96c2e19f548f8320bc3e49e2a6ac3d55a068
ashylen <ashylen@o2.pl>
2020-06-27 16:20:11 +0200

    Added error handling

--- src/App.js
+++ src/App.js
@@ -1,34 +1,65 @@
 import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 
+const url = (token) => `https://blynk-cloud.com/${token}/project`;
+
 function App() {
-  const url =
-    "https://blynk-cloud.com/ee4ea75d8dc543df979af396f9fffe22/project";
   const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [input, setInput] = useState({});
+  const [token, setToken] = useState("ee4ea75d8dc543df979af396f9fffe22");
 
   const getData = async () => {
     try {
-      const response = await axios.get(url);
+      setIsLoading(true);
+      const response = await axios.get(url(token));
       setData(response.data);
     } catch (err) {
-      throw err;
+      console.log(err.response);
+      setError(JSON.stringify(err));
+    }
+    setIsLoading(false);
+  };
+
+  const handleInputChange = (e) =>
+    setInput({
+      ...input,
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+
+  const handleButtonClick = (e) => {
+    if (input && input.token) {
+      setToken(input.token);
     }
   };
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [token]);
+
   return (
     <div className="App">
       <header className="App-header">
+        <div className="form-wrapper">
+          <input
+            type="text"
+            name="token"
+            onChange={handleInputChange}
+            placeholder="Wprowadź token"
+          />
+          <button onClick={handleButtonClick}>Pobierz dane</button>
+        </div>
+
         <h1>Dane</h1>
         {data ? (
           <div>
             {data &&
               data.widgets &&
               data.widgets.map((widget) => (
-                <p>
+                <p key={widget.id}>
                   {widget.label}: {widget.value}
                   {widget.valueFormatting.slice(-2)}
                 </p>
@@ -37,6 +68,14 @@ function App() {
         ) : (
           <p>Nie znaleziono danych</p>
         )}
+        {isLoading && (
+          <div>
+            <p>Ładowanie...</p>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+        )}
+
+        {error && <span>{error}</span>}
       </header>
     </div>
   );
